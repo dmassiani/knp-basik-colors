36029b25493737df5bd31ec67c3ee772645e4867
dmassiani <davidmassianirennes@gmail.com>
2016-10-03 20:32:52 +0200

    add skins

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -1,3 +1,20 @@
 import './modules'
 
+import $ from 'jquery'
+
+window.jQuery = $;
+window.$ = $;
+require('jquery-migrate')
+
 console.log(`app.js has loaded!`)
+
+require.ensure([], function() {
+
+	$(function() {
+		$('.sidebar._toggler').on('click', function(e){
+			e.preventDefault()
+			$('.sidebar._wrapper').toggleClass('-active')
+		})
+	})
+
+})
---
+++ src/javascripts/modules/sidebar.js
@@ -0,0 +1,6 @@
+export default class Sidebar {
+  constructor(el) {
+    this.el = el
+    var that = el
+  }
+}
