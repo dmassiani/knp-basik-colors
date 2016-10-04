b98221802fe99bb8d683cf5edbf8db64cb8fa02b
dmassiani <davidmassianirennes@gmail.com>
2016-10-04 15:03:31 +0200

    prod

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -9,6 +9,7 @@ require('jquery-migrate')
 require.ensure([], function() {
 
 	$(function() {
+
 		$('.sidebar._toggler').on('click', function(e){
 			e.preventDefault()
 			$('.sidebar._wrapper').toggleClass('-active')
@@ -17,6 +18,25 @@ require.ensure([], function() {
 				$('.sidebar._wrapper').removeClass('-in-progress')
 			}, 300)
 		})
+
+		$.each( $('.selector li a'), function(i,e){
+			let el = $(e)
+			let color = el.data('color')
+			el.css({'background-color': '#' + color })
+			el.on('click', function(e){
+				e.preventDefault()
+				$('body').data('skin', el.data('skin') ).trigger('changeSkin')
+			})
+		})
+
+		// change Skin event
+		$(document).on('changeSkin', function(e){
+			let skin = $('body').attr('class').split(' ').pop()
+			if( skin != '-bg' )
+				$('body').removeClass(skin)
+			$('body').addClass($('body').data('skin'))
+		})
+
 	})
 
 })
