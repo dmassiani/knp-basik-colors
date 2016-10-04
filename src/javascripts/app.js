54b6a79665cd89a8df05c085572a1105ac5c509f
dmassiani <davidmassianirennes@gmail.com>
2016-10-04 11:58:34 +0200

    update

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -6,14 +6,16 @@ window.jQuery = $;
 window.$ = $;
 require('jquery-migrate')
 
-console.log(`app.js has loaded!`)
-
 require.ensure([], function() {
 
 	$(function() {
 		$('.sidebar._toggler').on('click', function(e){
 			e.preventDefault()
 			$('.sidebar._wrapper').toggleClass('-active')
+									.addClass('-in-progress')
+			setTimeout( function(){
+				$('.sidebar._wrapper').removeClass('-in-progress')
+			}, 300)
 		})
 	})
 
